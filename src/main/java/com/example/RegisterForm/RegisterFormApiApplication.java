8b5851d8004a347d9f1699f7926a2b401ca63037
JayaKishore535 <jayakishore7077@gmail.com>
2024-10-16 21:00:41 +0530

    first commit

---
+++ src/main/java/com/example/RegisterForm/RegisterFormApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.RegisterForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterFormApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterFormApiApplication.class, args);
+	}
+
+}
